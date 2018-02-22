26b84d8b3348310fa8b062d96883addadf12cf56
jericacoutinho <jerrii0102@gmail.com>
2018-02-22 15:22:14 -0500

    Done

--- src/ExpenseServlet.java
+++ src/ExpenseServlet.java
@@ -54,6 +54,7 @@ public class ExpenseServlet extends HttpServlet {
 			preparedStmt.execute();
 			ResultSet resultSet = preparedStmt.getResultSet();  //gets the result set
 			boolean recordfound= resultSet.next();
+			
 			if(recordfound)
 			{
 				String query1 = "update into Budget (username, budget) values(?,?)";
